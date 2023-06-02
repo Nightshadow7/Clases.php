3d4bcb8130cab1c818f595917e99c281f6f00e4d
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-06-02 13:28:57 -0500

    feat :rocket: apirest se añadio una api a la plantilla ya creada en el anterior filtro

---
+++ backend/controlers/campers.php
@@ -0,0 +1,38 @@
+<?php 
+
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+header('content-Type: application/json');
+require_once("../config/conectar.php");
+require_once("../modelo/camper.php");
+
+$camper = new Camper();
+
+$body = json_decode(file_get_contents("php://input"), true);
+
+switch ($_GET["op"]) {
+  case 'GetAll':
+    $datos = $camper -> get_camper();
+    echo json_encode($datos);
+    break;
+
+  case 'GetId':
+    $datos = $camper -> get_camper_id($body['id']);
+    echo json_encode($datos);
+    break;
+
+  case "insert":
+
+            $datos=$camper->insert_camper($body["id"], $body["imagen"], $body["nombre"], $body["edad"], $body["promedio"], $body["nivelCAmpus"], $body["nivelIngles"], $body["especialidad"], $body["direccion"], $body["celular"], $body["ingles"], $body["Ser"], $body["Review"], $body["Skills"],$body["Asitencia"]);
+            echo json_encode("insertado correctamente");
+
+        break;
+
+  default:
+    # code...
+    break;
+}
+
+
+?>
